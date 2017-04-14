13a311bafc188d18de29f1235290d0ada7f67b93
Sam <Sam@Sam-MacBook.local>
2017-04-14 17:28:56 +0800

    Recover 恢复上个版本

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -14,7 +14,7 @@ function component(BasicItemComponent){
 
       other.targetComponent = AntdDatePicker;
       if(_.isString(other.value)){
-        other.value = moment(other.value)
+        other.value = new moment(other.value,other.format || "YYYY-MM-DD")
       }
       other.type = "datepicker";
       this.propsAdapter(other);
--- src/components/RangePicker/index.jsx
+++ src/components/RangePicker/index.jsx
@@ -18,7 +18,7 @@ function component(BasicItemComponent){
       if(_.isArray(other.value)){
         other.value.forEach((v,k)=>{
           if(_.isString(v)){
-            other.value[k] = moment(v)
+            other.value[k] = new moment(other.value[k],other.format || "YYYY-MM-DD");
           }
         })
       }
