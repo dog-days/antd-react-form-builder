b0d92f565ae2ecdb1008f8682707adafa6910ae4
xianshannan <xianshannan@qq.com>
2018-02-06 10:28:44 +0800

    rewrite length rule

--- src/decorator/FormItemComponent.jsx
+++ src/decorator/FormItemComponent.jsx
@@ -1,75 +1,90 @@
-import React from 'react'
-import _ from 'lodash'
-import localeText from '../locale-provider/zh_CN'
-import { sprintf } from "sprintf-js"
+import React from 'react';
+import _ from 'lodash';
+import localeText from '../locale-provider/zh_CN';
+import { sprintf } from 'sprintf-js';
+import util from '../util';
+
+function lenthRule(min, max) {
+  let message;
+  return {
+    validator(rule, value, callback, source, options) {
+      const valueLength = util.strlen(value);
+      const errors = [];
+      let flag = true;
+      if (min && max) {
+        if (valueLength < min || valueLength > max) {
+          flag = false;
+          message = sprintf(locale.charactersBetwteen, min, max);
+        }
+      } else if (min) {
+        if (valueLength < min) {
+          flag = false;
+          message = sprintf(locale.charactersMin, min);
+        }
+      } else if (max) {
+        if (valueLength > max) {
+          flag = false;
+          message = sprintf(locale.charactersMax, max);
+        }
+      }
+      if (!flag) {
+        errors.push({
+          message,
+        });
+      }
+      callback(errors);
+    },
+  };
+}
 
 let contextTypes = {
   selectSourceData: React.PropTypes.object,
   antLocale: React.PropTypes.object,
-}
+};
 //所有表单项props适配器
-function propsAdapter(props){
+function propsAdapter(props) {
   let locale;
-  if(this.context.antLocale){
+  if (this.context.antLocale) {
     locale = this.context.antLocale.FormBuilderCommon;
-  }else {
+  } else {
     locale = localeText.FormBuilderCommon;
   }
   let {
     label,
-    formItemProps={},
+    formItemProps = {},
     value,
     required,
     boolean,
     min,
     max,
     onlyLetter,
-    rules=[],
+    rules = [],
   } = props;
-  if(!formItemProps.label){
+  if (!formItemProps.label) {
     formItemProps.label = label;
   }
-  min = parseInt(min,10);
-  max = parseInt(max,10);
-  function validateLength(){
-    if(min && max){
-      rules.unshift({
-        min,
-        max,
-        message: sprintf(locale.charactersBetwteen,min,max),
-      });
-    }else if(min){
-      rules.unshift({
-        min,
-        message: sprintf(locale.charactersMin,min),
-      });
-    }else if(max){
-      rules.unshift({
-        max,
-        message: sprintf(locale.charactersMax,max),
-      });
-    }
-  }
+  min = parseInt(min, 10);
+  max = parseInt(max, 10);
   //只有text类型有长度限制
-  switch(props.type){
+  switch (props.type) {
     default:
-    if(onlyLetter){
-      rules.unshift({
-        validator(rule, value, callback, source, options) {
-          var errors = [];
-          var pass = new RegExp("^[A-Za-z]*$").test(value);
-          if(!pass){
-            errors.push({
-              message: locale.charactersOnlyLetter,
-            })
-          }
-          callback(errors);
-        }
-      });
-    }
-    validateLength(); 
+      if (onlyLetter) {
+        rules.unshift({
+          validator(rule, value, callback, source, options) {
+            var errors = [];
+            var pass = new RegExp('^[A-Za-z]*$').test(value);
+            if (!pass) {
+              errors.push({
+                message: locale.charactersOnlyLetter,
+              });
+            }
+            callback(errors);
+          },
+        });
+      }
+      lenthRule(min, max);
   }
-  if(required){
+  if (required) {
     rules.unshift({
       required: true,
       message: (label || props.name) + locale.required,
@@ -78,20 +93,20 @@ function propsAdapter(props){
   props.rules = rules;
   props.formItemProps = formItemProps;
   //中间value传递处理
-  if(!props.storage){
-    if(this.storage){
+  if (!props.storage) {
+    if (this.storage) {
       props.storage = this.storage;
-    }else {
+    } else {
       props.storage = {
         value: value,
-      }
-      this.storage = props.storage; 
+      };
+      this.storage = props.storage;
     }
   }
   return props;
 }
 
-function formItemComponentDecorator(component){
+function formItemComponentDecorator(component) {
   component.prototype.propsAdapter = propsAdapter;
   component.contextTypes = contextTypes;
   return component;
